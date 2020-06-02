f41f7c8c0e4d146962c40ec58ad0f92676d821cc
Jose Miguel Iturralde <jmiturralde@miuandes.cl>
2020-06-02 01:33:22 -0400

    implementacion search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,28 @@
 class PagesController < ApplicationController
   def search
+
+    #user search
     @users = User.all.where("username LIKE ?", params[:q] + '%')
     @profile_pages = ProfilePage.all.where("name LIKE ?", params[:q] + '%')
-    @organizations = Organization.all.where("name LIKE ?", params[:q])
+    @distinct_profile_pages =[]
+    if @user != []
+      @profile_pages.each do |j|
+        @users.each do |k|
+          if j.user_id = k.id
+          else
+            @distinct_profile_pages.append(j)
+          end
+        end
+      end
+    else
+      @profile_pages.each do |j|
+        @distinct_profile_pages.append(j)
+      end
+    end
+    #organization search
+    @organizations = Organization.all.where("name LIKE ?", params[:q] + '%')
+
+    #event seratch
     @events = Event.all.where("title LIKE ? OR description LIKE ?", '%' + params[:q] + '%',  '%' + params[:q] + '%')
     @event_creators = []
     @users.each do |user|
@@ -11,11 +31,28 @@ class PagesController < ApplicationController
         @event_creators.append(t)
       end
     end
-    @organization_creators = []
+
+    @distinct_event_creators =[]
+    if @events!= []
+      @event_creators.each do |j|
+        @events.each do |k|
+          if (j.id == k.id)
+          else
+            @distinct_event_creators.append(j)
+          end
+        end
+      end
+    else
+      @event_creators.each do |j|
+        @distinct_event_creators.append(j)
+      end
+    end
+
+    @event_organization = []
     @organizations.each do |org|
       @temp = Event.where("organization_id = ?", org.id)
       @temp.each do |t|
-        @organization_creators.append(t)
+        @event_organization.append(t)
       end
       end
   end
