8fb6e90291f2e34607f515049d3eb59f78ca7188
martinmorenoc <mmoreno6@miuandes.cl>
2020-06-26 13:54:32 -0400

    last access impl

--- db/schema.rb
+++ db/schema.rb
@@ -205,6 +205,11 @@ ActiveRecord::Schema.define(version: 2020_06_23_010158) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "uid"
     t.boolean "terms"
     t.index ["email"], name: "index_users_on_email", unique: true
